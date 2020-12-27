2256d1c3d948578a20d7b15d401163597fc963dc
KLZ-0 <adrian@kalazi.com>
2020-12-27 13:39:24 +0100

    Add frequency mean and variance calculation + Adjust plot sizes

--- src/iss/audioproc.py
+++ src/iss/audioproc.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import iss.input
 from iss import plotter, operations
 
@@ -15,24 +17,30 @@ class AudioProcessor:
     def process_signals(self):
         the_chosen_one = self.on_frames[0]
         plotter.plot(the_chosen_one, "frame.pdf",
+                     figsize=(16, 3),
                      title="Frame",
                      xlabel="Time [ms]")
 
         wf = operations.center_clip_frame(the_chosen_one)
         plotter.plot(wf, "frame_clipped.pdf",
+                     figsize=(16, 3),
                      title="70% Center clipping",
                      xlabel="Samples")
 
         wf = operations.autocorrelate_frame(wf, 0)
         plotter.plot(wf, "frame_autocorrelated.pdf",
+                     figsize=(16, 3),
                      title="Autocorrelation",
                      xlabel="Samples")
 
-        freqs1 = operations.frames_to_base_frequency(self.off_frames, self.off_sr)
-        freqs2 = operations.frames_to_base_frequency(self.on_frames, self.on_sr)
-        plotter.plot_list([freqs1, freqs2], "base_frequencies.pdf",
-                          figsize=(16, 4),
+        freqs_off = operations.frames_to_base_frequency(self.off_frames, self.off_sr)
+        freqs_on = operations.frames_to_base_frequency(self.on_frames, self.on_sr)
+        plotter.plot_list([freqs_off, freqs_on], "base_frequencies.pdf",
+                          figsize=(16, 3),
                           title="Frame base frequencies",
                           xlabel="Frames",
                           ylabel="$f0$ [Hz]",
                           plot_labels=["Mask off", "Mask on"])
+
+        print("OFF", "\n- Mean:\t\t", np.mean(freqs_off), "\n- Variance:\t", np.var(freqs_off))
+        print("ON", "\n- Mean:\t\t", np.mean(freqs_on), "\n- Variance:\t", np.var(freqs_on))
