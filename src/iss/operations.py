880c77a779e29823cfb1f12849fda93b23861360
KLZ-0 <adrian@kalazi.com>
2020-12-30 13:53:57 +0100

    Change impulse response ifft + Probably wrong

--- src/iss/operations.py
+++ src/iss/operations.py
@@ -43,7 +43,7 @@ def logarithmize_spectrum(fft_frames):
 
 
 def impulse_response(frequency_response):
-    return ifft(frequency_response)
+    return ifft(frequency_response, n=320)[:160]
 
 
 def apply_filter(data, flt):
