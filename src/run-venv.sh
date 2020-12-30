c464d30fe38bb4c42863da42aac31fe3eba6c170
KLZ-0 <adrian@kalazi.com>
2020-12-30 13:52:48 +0100

    Fix run script

--- src/run-venv.sh
+++ src/run-venv.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Create a virtual environment
-python3 -m venv venv
+python3 -m venv venv || exit 1
 
 # Activate the virtualenv
-. venv/bin/activate
+. venv/bin/activate || exit 1
 
 # install the dependencies
-pip install -r requirements.txt
+pip install -r requirements.txt || exit 1
 
 # run the script
 python3 main.py
