36c78a6305dc1befe2bb6a33e738e36138e75c36
KLZ-0 <adrian@kalazi.com>
2020-12-31 23:20:37 +0100

    Finish until task 5

--- src/iss/plotter.py
+++ src/iss/plotter.py
@@ -98,7 +98,7 @@ def img(data, filename, figsize=(8, 4), title="Title"):
 
     plt.gca().set_title(title)
 
-    plt.colorbar(label="Spectral density [dB]")
+    plt.colorbar(label="Spectral power density [dB]")
 
     plt.savefig(path.join("outputs", filename))
 
